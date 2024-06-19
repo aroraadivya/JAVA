01995048fe6cb733c222aa40d721663441a927c5
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-06-19 10:39:55 +0530

    first

--- Myfirst.java
+++ Myfirst.java
@@ -1,9 +1,10 @@
-//skeleton of java programm 
-import java.lang.*;
+//skeleton of java programm , basic java program
+// import java.lang.*;
 
 class Myfirst
 {
-    public static void main(String arg[])
+    public static void main(String[] args)
+    //public static void main(String arg[])
     {
         System.out.println("Hello World");
     }
