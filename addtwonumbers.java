15e1bd8a95045444a103aeecec0f3bd2f1e74b16
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-23 13:48:02 +0530

    updated

--- addtwonumbers.java
+++
@@ -1,8 +0,0 @@
-public class addtwonumbers {
-    public static void main(String[] args) {
-                int num1 = 5;
-                int num2 = 10;
-                int sum = num1 + num2;
-                System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-        }
-}
