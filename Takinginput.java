a7a7394b922ae808de8e16091db241262b9e6463
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-27 18:37:49 +0530

    taking input

--- Takinginput.java
+++ Takinginput.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class Takinginput {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your lucky number");
+        int num_1 = sc.nextInt();
+
+        System.out.println("Lucky number is " + num_1);
+    }
 }
