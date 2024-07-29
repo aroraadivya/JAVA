2af83398ba99a91bcf9951f5a9950d5938c624e3
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-29 11:28:17 +0530

    simple interest

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
 public class SimpleInterest {
-    public static void main(String[] args) 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the principal amount: ");
+        float p = sc.nextFloat();
+
+        System.out.println("enter the rate of interest: ");
+        float r = sc.nextFloat();
+
+        System.out.println("enter the time period: ");
+        float t = sc.nextFloat();
+
+        float si = (p * r * t) / 100;
+
+        System.out.println("The simple interest is: " + si);
+    }
 }
