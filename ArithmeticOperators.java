a1da32d28067c8bee4cc67e9cf1819f6a3b6d12f
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-29 22:50:34 +0530

    arithmetic operators

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -1,3 +1,11 @@
 public class ArithmeticOperators {
-    
+    public static void main(String[] args) {
+        int p=20, q=10;
+
+        System.out.println(p+q);
+        System.out.println(p-q);
+        System.out.println(p*q);
+        System.out.println(p/q);
+        System.out.println(p%q);
+    }
 }
