6a1ce1caecc6e5ebf81a012f5547f9a265d46a19
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-29 22:53:38 +0530

    relatioonal operators

--- RelationalOperators.java
+++ RelationalOperators.java
@@ -1,3 +1,12 @@
 public class RelationalOperators {
-    
+    public static void main(String[] args) {
+        int p=10, q=15;
+
+        System.out.println(p == q);
+        System.out.println(p != q);
+        System.out.println(p <= q);
+        System.out.println(p >= q);
+        System.out.println(p < q);
+        System.out.println(p > q);
+    }
 }
