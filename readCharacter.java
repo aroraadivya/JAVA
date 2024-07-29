24efd846a2a4952f323b97c5b350c87b4ebcfc69
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-29 22:35:04 +0530

    read ch

--- readCharacter.java
+++ readCharacter.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class readCharacter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        System.out.println("Character is " +ch);
     }
     
 }
