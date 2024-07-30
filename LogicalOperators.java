c920c4dc67d210bc5d3cb0facef67b880a052fc4
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-30 10:42:39 +0530

    upd logical operators

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -1,3 +1,18 @@
 public class LogicalOperators {
-    
+    public static void main(String[] args) {
+        int p=15, q=18, r=5;
+
+        // && operator(Logical AND)
+        System.out.println((p>q) && (p>r));
+        System.out.println((p>q) && (p<r));
+
+        // || oprrator(Logical OR)
+        System.out.println((r<q) || (p<q));
+        System.out.println((p>q) || (q>r));
+        System.out.println((p<q) || (q<r));
+
+        //! operator(Logical NOT)
+        System.out.println(!(p == q));
+        System.out.println(!(p>q));
+    }
 }
