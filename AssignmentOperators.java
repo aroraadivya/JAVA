12910262c17dbe7abe1a9a4ce60f0c8ae0f3194c
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-30 10:49:22 +0530

    Assignment operators update

--- AssignmentOperators.java
+++ AssignmentOperators.java
@@ -1,3 +1,30 @@
 public class AssignmentOperators {
-    
+    public static void main(String[] args) {
+        int p=10;
+        int q;
+
+        // =
+        q=p;
+        System.out.println(q); //1o
+
+        //  +=
+        p += q;
+        System.out.println(p); //20
+
+        // -=
+        p -= q;
+        System.out.println(p); //10
+
+        // *=
+        p *= q;
+        System.out.println(p); //100
+
+        // /=
+        p /= q;
+        System.out.println(p); //10
+        
+        // %=
+        p %= q;
+        System.out.println(p); //0
+    }
 }
