a525fc645d2fd06eb89759ad182256e3433fea23
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-30 11:55:47 +0530

    bitwise operator upd

--- BitwiseOperators.java
+++ BitwiseOperators.java
@@ -1,3 +1,12 @@
 public class BitwiseOperators {
-    
+    public static void main(String[] args) {
+        int p=9, q=10;
+
+        System.out.println(p | q);
+        System.out.println(p & q);
+        System.out.println(p ^ q);
+
+        System.out.println(q >> 1);
+        System.out.println(q >> 2);
+    }
 }
