9f8cdfb8a59634a359c95012b94055042958b453
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-07-30 11:28:24 +0530

    unary operators upd

--- UnaryOperators.java
+++ UnaryOperators.java
@@ -1,3 +1,20 @@
 public class UnaryOperators {
-    
+    public static void main(String[] args) {
+        int p=5, q=5;
+
+        System.out.println(p++);
+        System.out.println(p);
+
+        System.out.println(++q);
+        System.out.println(q);
+
+        int x=p++;
+        int y=++q;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        System.out.println(p);
+        System.out.println(q);
+    }
 }
