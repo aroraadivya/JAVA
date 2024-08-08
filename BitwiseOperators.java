ebaeb1ba2f9ca6ca973493bead0711f82a1dff08
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-08-08 20:43:48 +0530

    update

--- BitwiseOperators.java
+++ BitwiseOperators.java
@@ -9,5 +9,12 @@ public class BitwiseOperators {
         System.out.println(q >> 1);
         System.out.println(q >> 2);
 
+        System.out.println(p | q);
+        System.out.println(p & q);
+        System.out.println(p ^ q);
+
+        System.out.println(q >> 1);
+        System.out.println(q >> 2);
+
     }
 }
