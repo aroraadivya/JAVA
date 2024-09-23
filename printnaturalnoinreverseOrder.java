a7d11d345efa6da3aa864c3679f8f7f2f94ae96f
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-23 20:17:04 +0530

    upd

--- printnaturalnoinreverseOrder.java
+++ printnaturalnoinreverseOrder.java
@@ -5,7 +5,7 @@ public class printnaturalnoinreverseOrder {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int num=n; num>=1; num--){
+        for(int num=1; num>=n; num--){
             System.out.println(num);
         }
     }
