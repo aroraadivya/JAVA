51aa8b64f421ae207ec9fcae095cf4618765f36b
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-23 19:28:37 +0530

    sum of first n numbers

---
+++ SumOfFirstNnumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfFirstNnumbers {
+    public static void main(String[] args) {
+        int num = 1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        while(num<=n){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
