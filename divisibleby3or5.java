f515479969e13d3bc4694d0fc4c85d00d04b460e
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-23 19:10:07 +0530

    divisibke by 3 or 5

---
+++ divisibleby3or5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class divisibleby3or5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+
+        if(value % 3 == 0 || value % 5 == 0){
+            System.out.println("ans " + value);
+        }
+    }
+}
