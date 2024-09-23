30cb3643d609082032af1ae71fb359f7facf2997
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-23 19:05:37 +0530

    conditional operator

---
+++ Conditionaloperators.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+public class Conditionaloperators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+
+        if(value % 2 ==0 && value % 3==0){
+            System.out.println("ans" + value);
+        }
+    }
+}
