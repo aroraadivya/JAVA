5ec17b59cf1f9af11f709b8733f3a6a9e4dd996b
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-10-22 00:05:38 +0530

    updates
    create sc

---
+++ streamOfNums.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class streamOfNums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        
+    }
+}
