976175cff9d7416e3d7c22f790e2d22a1fc6294c
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-13 15:56:28 +0530

    Refactor StreamOfNums class: update variable naming, implement do-while loop for sum calculation, and remove commented-out code

--- streamOfNums.java
+++ streamOfNums.java
@@ -1,10 +1,21 @@
 import java.util.Scanner;
 
-public class streamOfNums {
+public class StreamOfNums {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        int sum=0;
 
+        do{
+            sum += num;
+            num = sc.nextInt();
+        } while(num!= -1);
         
+        // while(num!= -1){
+        //     sum += num;
+        //     num = sc.nextInt();
+        // }
+        System.out.println("Sum of all numbers is: " + sum);
     }
 }
+  
