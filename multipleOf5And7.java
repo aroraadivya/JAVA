dfb4ba429345d18b37b6af68d49a89e33aa6e74d
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-13 16:03:42 +0530

    Add multipleOf5And7 class to find the first number divisible by both 5 and 7

---
+++ multipleOf5And7.java
@@ -0,0 +1,13 @@
+public class multipleOf5And7 {
+    public static void main(String[] args) {
+        
+        int num = 1;
+        while(true){
+            if((num%5==0) && (num%7==0)){
+                System.out.println(num);
+                break;
+            }
+            num++;
+        }
+    }
+}
