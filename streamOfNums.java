08c8a6f81a469d3c486b7dddef2ce7fd80e437a1
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-13 15:46:33 +0530

    Remove unnecessary blank line in streamOfNums.java

--- streamOfNums.java
+++ streamOfNums.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class streamOfNums {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int num = sc.nextInt();
 
         
